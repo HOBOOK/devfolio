7c8ed8822f7702f705957c7aca6fdf67aa3b83d0
ghpark <pkh879@gmail.com>
2021-03-28 19:47:17 +0900

    [CREATE] 프로젝트 생성

---
+++ backend/src/test/java/com/ghpark/devfolio/DevfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghpark.devfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
